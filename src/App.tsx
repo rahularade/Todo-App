f4a2150ed1aabb669190e590c6fc860bb2a00815
Rahul Arade <araderahul9196@gmail.com>
2026-01-22 10:50:04 +0530

    Replace vite.svg with todo.svg as the favicon

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,9 @@ import { TodoContextProvider } from "./context/TodoContex";
 function App() {
     return (
         <div className="w-full min-h-dvh p-5 relative flex flex-col items-center bg-background font-poppins">
-            <a href="https://github.com/rahularade/Todo-App" target="_blank">
-                <h1 className="text-4xl font-medium text-heading my-5">
-                    Just Do It!
-                </h1>
-            </a>
+            <h1 className="text-4xl font-medium text-heading my-5">
+                Just Do It!
+            </h1>
             <TodoContextProvider>
                 <div className="bg-foreground py-5 px-4 pb-2.5 rounded-lg w-96">
                     <AddTodo />
